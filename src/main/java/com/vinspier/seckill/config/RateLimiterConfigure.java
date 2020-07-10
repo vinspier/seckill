10cde8bb172795eaf3ed14dac89d73d2991436fb
vinspier <17767160232@163.com>
2020-07-10 23:57:18 +0800

    添加 google guava RateLimiter 每秒限流数 配置

---
+++ src/main/java/com/vinspier/seckill/config/RateLimiterConfigure.java
@@ -0,0 +1,24 @@
+package com.vinspier.seckill.config;
+
+import com.google.common.util.concurrent.RateLimiter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 使用google guava 的RateLimiter 限流
+ * */
+
+@Configuration
+public class RateLimiterConfigure {
+
+    @Autowired
+    private CustomizeProperties customizeProperties;
+
+    @Bean
+    public RateLimiter rateLimiter(){
+        RateLimiter rateLimiter = RateLimiter.create(customizeProperties.getPayedWaited());
+        return rateLimiter;
+    }
+
+}
